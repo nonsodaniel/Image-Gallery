8a8c797be24c32d4554380f72fd191b7c63aa201
nonsodaniel <nonsodaniel07@gmail.com>
2020-07-27 04:36:50 +0200

    Added layout

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react'
 import Navbar from '../layout/Navbar'
 
+import './home.scss'
+
 export class Home extends Component {
     render() {
         return (
-            <div className="container-fluid">
-                <div className="wrapper">
+            <div className="container-wrap">
+                <div className="home-wrapper">
                     <Navbar />
+                </div>
+
+                <div className="bottom-wrap">
+                    <div className="container-fluid">
+                        <div className="text">
+                            <h1>Picture name by <h6>(Nonso Daniel)</h6>  </h1>
+                            <p>This is some badass picture you are gonna love</p>
+                        </div>
+                        <div className="action">
+
+                        </div>
+                    </div>
                 </div>
 
             </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,12 +6,12 @@ const Navbar = () => {
     return (
         <header>
             <div className="navbar-wrap">
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg text-white">
                     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-                        <Link className="navbar-brand" to="#">Hidden brand</Link>
+                        <Link className="navbar-brand text-white brand-name" to="#">Nonso Photography</Link>
                         <ul className="navbar-nav ml-auto mt-2 header-nav">
                             <li className="nav-item active">
                                 <Link className="nav-link" to="#">ABOUT <span className="sr-only">(current)</span></Link>
